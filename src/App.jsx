7999815b674d6f49378722f6eb451f7e5147831b
Rayan200013 <rayan.bouezzeddine13@gmail.com>
2026-03-15 09:28:10 +0200

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <>
+    <NavBar />
+        <h1 class="text-3xl font-bold underline text-indigo-700">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
