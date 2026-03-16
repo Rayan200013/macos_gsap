30245d68a1ea54eab88398506001e3111ab29d05
Rayan200013 <rayan.bouezzeddine13@gmail.com>
2026-03-16 07:54:29 +0200

    Added models, converted them form gltf into jsx + fixed scale and color between the two models + added the lights

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+// register GSAP in order to work
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   return (
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import useMacbookStore from '../store'
 import clsx from 'clsx';
+import { Canvas } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14';
+import StudioLights from './three/StudioLights';
+import ModelSwitcher from './three/ModelSwitcher';
+import { useMediaQuery } from 'react-responsive';
 
 const ProductViewer = () => {
 
     const {color, scale, setColor, setScale } = useMacbookStore();
+
+    const isMobile = useMediaQuery({query: '(max-width: 1024px)'})
 
   return (
     <section id='product-viewer'>
@@ -39,7 +47,18 @@ const ProductViewer = () => {
                 </div>
             </div>
         </div>
-        <p className='text-white text-4xl'>Render Canvas</p>
+        
+        <Canvas id='canvas' camera={{position: [0, 2, 5], fov:50, near:0.1, far: 100 }}>
+            {/* <Box position={[0, 1, 0]} scale={10 * scale} material-color={color}></Box> */}
+            <StudioLights />
+            
+
+            {/* For zoom we use OrbitControls */}
+            {/* <OrbitControls enableZoom={false} />  */}
+
+            <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
+
+        </Canvas>
     </section>
   )
 }
