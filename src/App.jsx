50762e54a070abdbca5b09cf0043dcbd2f2c43e6
Rayan200013 <rayan.bouezzeddine13@gmail.com>
2026-03-17 08:25:28 +0200

    Added all rest components for the landing page + Implemented Performance section with GSAP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Showcase from './components/ShowCase'
+import Performance from './components/Performance'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
 
 // register GSAP in order to work
 gsap.registerPlugin(ScrollTrigger)
@@ -16,8 +19,12 @@ function App() {
     <Hero />
     <ProductViewer />
     <Showcase />
+    <Performance />
+    <Features />
+    <Highlights />
+    <Footer />
     </>
   )
 }
 
-export default App
+export default App;
